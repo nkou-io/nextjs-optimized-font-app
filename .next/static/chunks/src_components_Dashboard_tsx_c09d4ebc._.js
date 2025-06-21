15d55fccf64acbaee78d14208b8166ceb38c8815
nkou-io <nkoudefiosirus@gmail.com>
2025-06-21 15:39:16 +0200

    Add .next/static/chunks/src_components_Dashboard_tsx_c09d4ebc._.js 1750513155370

---
+++ .next/static/chunks/src_components_Dashboard_tsx_c09d4ebc._.js
@@ -0,0 +1,17 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/components/Dashboard.tsx [app-client] (ecmascript, next/dynamic entry, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/node_modules_d22ab973._.js",
+  "static/chunks/src_26f1be78._.js",
+  "static/chunks/src_components_Dashboard_tsx_da60764f._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/components/Dashboard.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}}),
+}]);
